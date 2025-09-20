cf0733aae7b29bff040f732841362a0ea51843fc
AEROSPACE444 <tanatswakole@gmail.com>
2025-09-20 11:00:15 +0200

    Update settingss.js

--- settingss.js
+++ settingss.js
@@ -1,7 +1,7 @@
 
 module.exports = {
-  SESSION_ID: "",  // add your Session Id here
-  OWNER_NUMBER: "", // put your phone number here
+  SESSION_ID: "malvin~MvEwxa6L#oFbV-_sEbWWykljzrfXLiwrXZvhmm4_vtvTn4n6Nny8",  // add your Session Id here
+  OWNER_NUMBER: "263780492124", // put your phone number here
   PREFIX: ".", // prefix (e.g., ., /, !, *)
   TIMEZONE: "Africa/Kampala" //put your country timeZone....leave blank if u don't know.
 };
